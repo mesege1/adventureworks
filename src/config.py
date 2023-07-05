102e3dabaf2b2e0467391b1747757a2d2d6ab22b
sys-usr <london.thomson.merriman@gmail.com>
2023-07-05 17:21:37 -0400

    question 2 sql query

---
+++ src/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+
+
+if __name__ == "__main__":
+    print(
+        POSTGRES_PASSWORD,
+        POSTGRES_USER,
+        POSTGRES_HOST,
+        POSTGRES_PORT
+    )
+
